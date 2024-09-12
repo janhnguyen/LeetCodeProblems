6ab5f581e7a91fcdd33400340a06c0a3f04f226d
janhnguyen <nguyenjanh01@gmail.com>
2024-09-12 16:04:13 -0400

    twoSum solved

---
+++ solutions/twoSum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        //base case
+        if (nums.length == 2) {
+            return new int[]{0,1};
+        }
+
+        //n(n-1) runtime
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i,j};
+                }
+            }
+        }
+
+        return null;
+
+    }
+}
